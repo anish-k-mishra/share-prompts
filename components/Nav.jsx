f607e6a06de7f01a1423efbbf696f44e7b48bf34
Anish Mishra <anishkumarc92@gmail.com>
2024-08-29 03:15:13 +0530

    authentication with google

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,21 +6,22 @@ import { useState, useEffect } from "react"
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data:session} = useSession()
 
-  const [providers , setProviders ] = useState(null) 
+  const [providers , setProviders ] = useState(null) ;
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(()=>{
-    const setProviders = async () =>{
+    const setUpProviders = async () =>{
       const response = await getProviders();
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap=2 flex-center">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image 
           src="/assets/images/logo.svg"
           alt="Logo"
@@ -33,7 +34,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5 ">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -45,7 +46,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image 
-                src="/assets/images/logo.svg"
+                src= {session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -74,16 +75,45 @@ const Nav = () => {
       {/* Mobile Navigation */}
 
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image 
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
               alt="profile"
-              onClick={()=>{}}
+              onClick={()=>setToggleDropdown((prev)=>!prev)}
             />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={()=>setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={()=>setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button 
+                  type="button"
+                  onClick={()=>{
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ): (
           <>
